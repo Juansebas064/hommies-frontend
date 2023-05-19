58fb3c880118c94eed148b663dc246bb8df27c92
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-05-19 18:32:32 -0500

    Sección de actividades en dashboard

---
+++ src/components/Dashboard/Activities.jsx
@@ -0,0 +1,9 @@
+export default function Activities() {
+  return (
+    <div className="basis-1/3 border-solid bg-gray-200 border-gray-800 rounded-xl shadow-xl m-5 ml-1">
+      <h1 className="text-4xl text-center">
+        Actividades
+      </h1>
+    </div>
+  )
+}
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import MapView from './MapView'
+import Activities from './Activities'
 
 export default function Dashboard() {
   return (
-    <MapView />
+    <div className="flex p-0">
+      <MapView />
+      <Activities />
+    </div>
   )
 }
