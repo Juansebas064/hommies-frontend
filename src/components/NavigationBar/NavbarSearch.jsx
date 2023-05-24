a6d32253232091113e5f693f83012bbe47ca7f83
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-05-24 13:08:44 -0500

    Barra de búsqueda ahora redimensionable

--- src/components/NavigationBar/NavbarSearch.jsx
+++ src/components/NavigationBar/NavbarSearch.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function NavbarSearch() {
   return (
-    <div className="relative">
+    <div className="ml-6 relative flex-grow md:max-w-md">
       <input
         type="text"
         placeholder="Buscar..."
-        className="pl-10 pr-4 py-2 w-96 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="pl-10 pr-4 py-2 w-full  rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
       <svg
         className="absolute left-3 top-2.5 h-5 w-5 text-gray-400"
