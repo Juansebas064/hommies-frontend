0af4dc933f7471088083ce59462584c0ba53f0d8
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-05-27 18:38:28 -0500

    Events renombrado de acuerdo a lo que muestra

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import MapView from './MapView'
-import Activities from './Events'
+import EventsAndPlaces from './EventsAndPlaces'
 
 export default function Dashboard() {
   return (
     <div className="flex flex-col lg:flex-row mx-auto p-0">
       <MapView />
-      <Activities />
+      <EventsAndPlaces />
     </div>
   )
 }
