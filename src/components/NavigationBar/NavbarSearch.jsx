72743aa7d97f14701e667e6b353e6ca835e2b360
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-05-27 18:36:41 -0500

    Cambios en el posicionamiento del svg y font-size

--- src/components/NavigationBar/NavbarSearch.jsx
+++ src/components/NavigationBar/NavbarSearch.jsx
@@ -2,14 +2,9 @@ import React from "react";
 
 function NavbarSearch() {
   return (
-    <div className="ml-6 relative flex-grow md:max-w-md">
-      <input
-        type="text"
-        placeholder="Buscar..."
-        className="pl-10 pr-4 py-2 w-full  rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
+    <div className="flex ml-6 relative flex-grow md:max-w-md">
       <svg
-        className="absolute left-3 top-2.5 h-5 w-5 text-gray-400"
+        className="absolute self-center ml-3 h-5 w-5 text-gray-400"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
@@ -22,6 +17,11 @@ function NavbarSearch() {
           d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
         />
       </svg>
+      <input
+        type="text"
+        placeholder="Buscar..."
+        className="pl-10 pr-4 py-2 w-full rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm"
+      />
     </div>
   );
 }
