06ffba2116499e891790b87f830d0d22996aeae6
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-05-30 17:01:40 -0500

    Función en comentario para hacer get de lugares

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { useState } from 'react';
 import MapView from './MapView'
 import EventsAndPlaces from './EventsAndPlaces'
 
 export default function Dashboard() {
+
+  // Estados: 
+  // Lugares de la ciudad (datos de la bd)
+  const [places, setPlaces] = useState(null);
+
+  // Ejecutar fetchPlaces() al renderizar componente
+  // useEffect(() => {
+  //   fetchPlaces();
+  // }, []);
+
+  // Hacer la petición de la información a la base de datos
+  // const fetchPlaces = async () => {
+  //   try {
+  //     const response = await axios.get('Route/to/fetch/places'); // Ruta de la API en el backend
+  //     // console.log(response.data)
+  //     setPlaces(response.data); // Almacenar los datos en el estado local
+  //   } catch (error) {
+  //     console.error('Error al realizar la solicitud al backend:', error);
+  //   }
+  // };
+
   return (
     <div className="flex flex-col lg:flex-row mx-auto p-0">
       <MapView />
