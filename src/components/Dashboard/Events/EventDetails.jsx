6654cb3c195a35e0ef2c4e4421f48245a065e66a
casariz <danielfelipedfco@gmail.com>
2023-06-05 11:40:59 -0500

    Boton para ver participantes en eventos

--- src/components/Dashboard/Events/EventDetails.jsx
+++ src/components/Dashboard/Events/EventDetails.jsx
@@ -1,34 +1,63 @@
-import getEventStatus from "../../../utils/getEventStatus.js"
-import { clockSVG, calendarSVG, close } from "../../../utils/svgs"
+import { useState } from "react";
+import getEventStatus from "../../../utils/getEventStatus.js";
+import { clockSVG, calendarSVG, close, ellipsis } from "../../../utils/svgs";
 
-export default function EventDetails({ selectedEvent, setSelectedEvent, eventStatus }) {
-
+export default function EventDetails({
+  selectedEvent,
+  setSelectedEvent,
+  eventStatus,
+}) {
   if (!selectedEvent) {
-    return null
+    return null;
   }
 
-  const status = getEventStatus(selectedEvent.fecha.substring(0, 10), selectedEvent.hora_inicio.substring(0, 5), selectedEvent.hora_final.substring(0, 5))
+  const status = getEventStatus(
+    selectedEvent.fecha.substring(0, 10),
+    selectedEvent.hora_inicio.substring(0, 5),
+    selectedEvent.hora_final.substring(0, 5)
+  );
+
+  const [modifyEvent, setModifyEvent] = useState(false);
 
   return (
-
     // Cuadro de diálogo modal
     <div className="fixed z-20 bg-[rgba(0,0,0,0.2)] h-full w-full left-0 top-0 overflow-auto flex justify-center items-center">
-
       {/* Card */}
       <div className="z-30 bg-white rounded-[14px] relative min-w-[250px] max-w-[350px] sm:max-w-[600px] max-h-[50vh] sm:max-h-[60vh] px-3 sm:px-7 flex-grow overflow-auto">
-
         {/* Botón cerrar */}
         <span
           onClick={() => {
-            setSelectedEvent(null)
+            setSelectedEvent(null);
           }}
-          className="text-base font-bold absolute top-2 right-2 rounded-3xl cursor-pointer hover:scale-[0.8] hover:transition-all duration-500 p-1 hover:bg-red-500">
+          className="text-base font-bold absolute top-2 right-2 rounded-3xl cursor-pointer hover:scale-[0.8] hover:transition-all duration-500 p-1 hover:bg-red-500"
+        >
           {close(22)}
         </span>
 
+        <span
+          onClick={() => {
+            setModifyEvent(!modifyEvent);
+          }}
+          className="text-base font-bold absolute top-2 left-2 rounded-3xl cursor-pointer hover:scale-[0.8] hover:transition-all duration-500 p-1 hover:bg-indigo-400"
+        >
+          {ellipsis(26)}
+        </span>
+
+        {modifyEvent && (
+          <div className="absolute top-7 left-4 z-50 mt-2 bg-white divide-y divide-gray-200 rounded-md shadow-lg overflow-hidden">
+            <button className="block px-4 py-2 text-sm w-full text-gray-700 hover:bg-indigo-400 hover:duration-500 z-50">
+              Modificar
+            </button>
+            <button className="block px-4 py-2 text-sm w-full text-gray-700 hover:bg-red-600 hover:duration-500 hover:text-white z-50">
+              Eliminar
+            </button>
+          </div>
+        )}
+
+
+
         {/* Contenido del evento */}
         <div className="py-6 px-[8px] grid grid-cols-2">
-
           {/* Nombre del evento */}
           <p className="text-lg font-bold text-center col-span-2 mb-3">
             {`${selectedEvent.nombre}`}
@@ -44,9 +73,7 @@ export default function EventDetails({ selectedEvent, setSelectedEvent, eventSta
           <p className="text-center font-bold">Temática</p>
 
           {/* Estado del evento */}
-          <p className={
-            `${eventStatus[status].color} mb-8 text-center`
-          }>
+          <p className={`${eventStatus[status].color} mb-8 text-center`}>
             {`● ${eventStatus[status].text}`}
           </p>
 
@@ -60,20 +87,30 @@ export default function EventDetails({ selectedEvent, setSelectedEvent, eventSta
           {/* Fecha del evento */}
           <p className="text-sm text-center lg:text-center">
             <span className="mr-[6px]">{calendarSVG(20)}</span>
-            <span className="align-middle">{`${selectedEvent.fecha.substring(0, 10)}`}</span>
+            <span className="align-middle">{`${selectedEvent.fecha.substring(
+              0,
+              10
+            )}`}</span>
           </p>
 
           {/* Hora del evento */}
           <p className="text-sm text-center lg:text-center">
             <span className="mr-[6px]">{clockSVG(18)}</span>
-            <span className="align-end">{`${selectedEvent.hora_inicio.substring(0, 5)} - ${selectedEvent.hora_final.substring(0, 5)}`}</span>
+            <span className="align-end">{`${selectedEvent.hora_inicio.substring(
+              0,
+              5
+            )} - ${selectedEvent.hora_final.substring(0, 5)}`}</span>
           </p>
 
+
         </div>
+
+        <details className="block w-full max-w-xs mx-auto mb-7 bg-indigo-500 hover:bg-indigo-600 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">
+              <summary>¿Buscas a alguien?</summary>
+              <p>Smith</p>
+              <p>Casariz</p>
+        </details>
       </div>
     </div>
-  )
+  );
 }
-
-
-
--- src/utils/svgs.jsx
+++ src/utils/svgs.jsx
@@ -64,3 +64,16 @@ export const buttonPrevious = (size) => (
     <path d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12-5.373-12-12-12-12 5.373-12 12zm7.58 0l5.988-5.995 1.414 1.416-4.574 4.579 4.574 4.59-1.414 1.416-5.988-6.006z" />
   </svg>
 )
+
+export const ellipsis = (size) => (
+  <svg xmlns="http://www.w3.org/2000/svg" 
+    width={`${size}`}
+    height={`${size}`}
+    fill="none" 
+    viewBox="0 0 24 24" 
+    stroke="currentColor">
+  <path d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+</svg>
+
+
+)
