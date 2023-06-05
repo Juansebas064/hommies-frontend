abf5b6bf1eaf5e630c8aaea487f53af3681bf1fa
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-06-05 10:09:56 -0500

    Creado archivo para llamar eventos de la bd y cambios en lista de eventos

--- src/components/Dashboard/EventsAndPlaces.jsx
+++ src/components/Dashboard/EventsAndPlaces.jsx
@@ -1,14 +1,14 @@
-import axios from "axios";
 import { useState, useEffect } from "react";
 import EventsList from "./Events/EventsList";
 import PlacesList from "./Places/PlacesList";
 import EventsFilters from "./Events/EventsFilters";
+import { fetchEvents } from "../../utils/fetchEvents";
 
 export default function EventsAndPlaces() {
 
   // Estados: 
   // Actividades en la ciudad (datos de la bd)
-  const [events, setEvents] = useState(null);
+  // const [events, setEvents] = useState(null);
 
   // Pestaña activa (eventos o lugares)
   const [activeTab, setActiveTab] = useState('events')
@@ -19,25 +19,14 @@ export default function EventsAndPlaces() {
 
 
   // Funciones del componente:
-  // Hacer la petición de la información de eventos a la base de datos
-  const fetchEvents = async () => {
-    try {
-      const response = await axios.get('http://localhost:5000/api/evento/consultar'); // Ruta de la API en el backend
-      console.log(response.data.rows)
-      setEvents(response.data.rows); // Almacenar los datos en el estado local
-    } catch (error) {
-      console.error('Error al realizar la solicitud al backend:', error);
-    }
-  };
-
-  const haveToken = localStorage.getItem('token');
-
-
-  if (haveToken != null) {
-  // Ejecutar fetchEvents() al renderizar componente
-  useEffect(() => {
-    fetchEvents();
-  }, []);
+  // Hacer la petición de la información de eventos a la base de datos con fetchEvents()
+  // useEffect(() => {
+  //   async function eventsResponse() {
+  //     const eventsResponse = await fetchEvents()
+  //     setEvents(eventsResponse)
+  //   }
+  //   eventsResponse()
+  // }, []);
   // Fin funciones del componente
 
   return (
@@ -67,32 +56,14 @@ export default function EventsAndPlaces() {
 
             {/* Contenedor eventos */}
             <div className="relative flex flex-col items-center text-sm lg:px-0 overflow-hidden">
-              {events ?
-                /* Panel de eventos */
-                < EventsList events={events} activeFilter={activeFilter} />
-                :
-                <div className="flex-grow flex flex-col items-center justify-center text-[30px] min-h-[150px] text-sm">
-                  <p className="text-center">No hay eventos para mostrar</p>
-                </div>
-              }
+              < EventsList activeFilter={activeFilter} />
             </div>
           </>
           :
           <PlacesList />
       }
-
     </div >
   )
-    } else {
 
-       
-  return (
-    <div>
-      <p>inicie sesion pues ombe</p>
-      <p>no has iniciado sesion</p>
-    </div>
-  );
-
-    }
 }
 
--- src/components/Profile/ProfileConfig.jsx
+++ src/components/Profile/ProfileConfig.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import History from './History'
+import { useEffect, useState } from "react";
+import EventsList from "../Dashboard/Events/EventsList";
 
 const ProfileConfig = () => {
 
-  // const handleClick = (e) => {
+  // function handleEditarPerfil(e) {
   //   e.preventDefault();
   //   // Aquí puedes realizar las acciones que desees sin recargar la página
   //   console.log('Botón clicado');
@@ -73,116 +73,122 @@ const ProfileConfig = () => {
   }
 
   return (
-    <div className="container mx-auto px-0 py-28">
-      <div className="grid lg:grid-cols-2">
-        <div className="px-16 pt-10 pb-4 bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-[80%]  max-w-[600px] overflow-hidden">
-          <h3 className="font-bold text-3xl text-gray-900 text-center mb-7">Configuración del perfil</h3>
 
-          <form>
-            <div className="w-full px-3 mb-5">
-              <label className="text-sm font-semibold px-1">
-                Nombre
-              </label>
+    // Contenedor principal
+    <div className="flex flex-col lg:flex-row lg:justify-evenly my-10">
 
-              <div className="flex">
-                <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                    />
-                  </svg>
-                </div>
-                <input
-                  type="text"
-                  className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
-                />
+      {/* Card configuración del perfil */}
+      <div className="px-12 pt-10 pb-4 bg-gray-100 text-gray-500 rounded-3xl shadow-xl mx-auto max-w-[600px] overflow-hidden flex-grow">
+        <h3 className="font-bold text-3xl text-gray-900 text-center mb-7">Configuración del perfil</h3>
+        <form>
+          <div className="w-full px-3 mb-5">
+            <label className="text-sm font-semibold px-1">
+              Nombre
+            </label>
+            <div className="flex">
+              <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                  />
+                </svg>
               </div>
+              <input
+                type="text"
+                className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+              />
             </div>
-
-            <div className="w-full px-3 mb-5">
-              <label className="text-sm font-semibold px-1">
-                Apellido
-              </label>
-
-              <div className="flex">
-                <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                    />
-                  </svg>
-                </div>
-                <input
-                  type="text"
-                  className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
-                />
+          </div>
+          <div className="w-full px-3 mb-5">
+            <label className="text-sm font-semibold px-1">
+              Apellido
+            </label>
+            <div className="flex">
+              <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                  />
+                </svg>
               </div>
+              <input
+                type="text"
+                className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+              />
             </div>
+          </div>
+          <div className="w-full px-3 mb-5">
+            <label className="text-sm font-semibold px-1">
+              Genero
+            </label>
+            <select
+              id="gender"
+              name="gender"
+              className="w-full pl-3 pr-3 py-3 bg-gray-50 border-2 border-gray-200 text-gray-900 text-sm rounded-lg focus:border-indigo-500 block"
+            >
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Other">Other</option>
+            </select>
+          </div>
+          <div className="w-full text-center mb-3">
+            <label className="font-semibold text-sm px-1">Cambia tus intereses</label>
+          </div>
+          <div className='border-4 border-gray-400 py-5 rounded-md w-full my-5 mx-auto flex justify-center flex-wrap'>
+            {intereses.map((elemento, index) => (
+              <span
+                key={index}
+                name={elemento.tematica}
+                className={`rounded-full px-2 py-1 my-1 mx-1 cursor-pointer select-none ${elemento.marcado ? 'bg-indigo-500 text-white' : 'bg-gray-200'}`}
+                onClick={() => handleEditarIntereses(index)}>
+                {elemento.tematica}
+              </span>
+            ))}
+          </div>
+          <button type="submit" className="inline-block w-[50%] max-w-xs mx-auto mt-7 bg-indigo-500 hover:bg-indigo-600 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">
+            Continuar
+          </button>
+          <button type="submit" className="inline-block w-[50%] max-w-xs mx-auto mt-7 hover:bg-gray-300 rounded-lg px-3 py-3 font-semibold"
+            onClick={(e) => {
+              e.preventDefault()
+              window.location.href = '/profile'
+            }}
+          >
+            Cancelar
+          </button>
+        </form>
+      </div>
 
-            <div className="w-full px-3 mb-5">
-              <label className="text-sm font-semibold px-1">
-                Genero
-              </label>
+      {/* Historial de actividades */}
+      <div className="px-10 pt-10 pb-4 text-gray-500">
 
-              <select
-                id="gender"
-                name="gender"
-                className="w-full pl-3 pr-3 py-3 bg-gray-50 border-2 border-gray-200 text-gray-900 text-sm rounded-lg focus:border-indigo-500 block"
-              >
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-                <option value="Other">Other</option>
-              </select>
-            </div>
-            <div className="w-full text-center mb-3">
-              <label className="font-semibold text-sm px-1">Cambia tus intereses</label>
-            </div>
-
-            <div className='border-4 border-gray-400 py-5 rounded-md w-full my-5 mx-auto flex justify-center flex-wrap'>
-              {intereses.map((elemento, index) => (
-                <span
-                  key={index}
-                  name={elemento.tematica}
-                  className={`rounded-full px-2 py-1 my-1 mx-1 cursor-pointer select-none ${elemento.marcado ? 'bg-indigo-500 text-white' : 'bg-gray-200'}`}
-                  onClick={() => handleEditarIntereses(index)}>
-                  {elemento.tematica}
-                </span>
-              ))}
-            </div>
-
-            <button type="submit" className="block w-full max-w-xs mx-auto mt-7 bg-indigo-500 hover:bg-indigo-600 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">
-              Continuar
-            </button>
-          </form>
-        </div>
-
-        <div className="px-16 pt-10 pb-4  bg-gray-100 text-gray-500 rounded-3xl shadow-xl">
-          
-                <h3 className="font-bold text-3xl text-gray-900 text-center mb-7">Historial de actividades</h3>
-                <div>
-                  <History/>
-                </div>
+        <h3 className="font-bold text-3xl text-gray-900 text-center mb-7">Historial de actividades</h3>
+        <p className="text-center">
+          Aqui se mostraran los eventos en los que has estado inscrito
+        </p>
+        <div className="relative flex flex-col items-center text-sm lg:px-0 overflow-hidden">
+          <EventsList activeFilter='inscrito' />
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
