91afb882c7cb025943379d95c564a451e0a4e7e7
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-06-11 18:11:52 -0500

    Corrección en propiedades de los elementos

--- src/components/Register/ProfilePreferences.jsx
+++ src/components/Register/ProfilePreferences.jsx
@@ -4,22 +4,22 @@ import axios from "axios";
 const ProfilePreferences = () => {
   const [backMessage, setBackMessage] = useState("");
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/api/ProfilePreferencesMessage")
-      .then((response) => {
-        setBackMessage(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:5000/api/ProfilePreferencesMessage")
+  //     .then((response) => {
+  //       setBackMessage(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }, []);
 
   return (
     <div>
-      <div class="min-w-screen min-h-screen bg-gray-100/50 flex items-center justify-center px-10 py-5">
-        <div class="bg-gray-100 text-gray-500 rounded-2xl shadow-xl w-full overflow-hidden">
-          <div class="md:flex w-full">
+      <div className="min-w-screen min-h-screen bg-gray-100/50 flex items-center justify-center px-10 py-5">
+        <div className="bg-gray-100 text-gray-500 rounded-2xl shadow-xl w-full overflow-hidden">
+          <div className="md:flex w-full">
             <div className="w-full rounded-2xl border-2 border-indigo-400">
               <p className="text-indigo-600 text-center font-extrabold text-4xl mt-8">
                 Escoge tus preferencias
@@ -27,7 +27,7 @@ const ProfilePreferences = () => {
 
               <div className="flex w-full items-center justify-center mt-6">
                 <div
-                  class="grid w-[500px] grid-cols-3 space-x-2 rounded-xl bg-gray-200 p-2"
+                  className="grid w-[500px] grid-cols-3 space-x-2 rounded-xl bg-gray-200 p-2"
 
                 >
                   <div>
@@ -35,12 +35,12 @@ const ProfilePreferences = () => {
                       type="radio"
                       name="option"
                       id="1"
-                      class="peer hidden"
+                      className="peer hidden"
 
                     />
                     <label
-                      for="1"
-                      class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white"
+                      htmlFor="1"
+                      className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white"
                     >
                       Hombre
                     </label>
@@ -51,11 +51,11 @@ const ProfilePreferences = () => {
                       type="radio"
                       name="option"
                       id="2"
-                      class="peer hidden"
+                      className="peer hidden"
                     />
                     <label
-                      for="2"
-                      class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white"
+                      htmlFor="2"
+                      className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white"
                     >
                       Mujer
                     </label>
@@ -66,11 +66,11 @@ const ProfilePreferences = () => {
                       type="radio"
                       name="option"
                       id="3"
-                      class="peer hidden"
+                      className="peer hidden"
                     />
                     <label
-                      for="3"
-                      class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white"
+                      htmlFor="3"
+                      className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white"
                     >
                       Prefiero no decirlo
                     </label>
