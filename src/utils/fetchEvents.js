f02f3172f2832b879a5ee0b9dbc697b60f8d4c04
Juan Felipe Arango <juan.felipe.arango@correounivalle.edu.co>
2023-06-12 15:44:57 -0500

    Cambio de end-point

--- src/utils/fetchEvents.js
+++ src/utils/fetchEvents.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchEvents = async () => {
   try {
-    const response = await axios.get('http://localhost:5000/api/evento/consultar'); // Ruta de la API en el backend
+    const response = await axios.get('http://localhost:5000/api/evento/obtenerC'); // Ruta de la API en el backend
     return response.data.rows; // Almacenar los datos en el estado local
   } catch (error) {
     console.error('Error al realizar la solicitud al backend:', error);
