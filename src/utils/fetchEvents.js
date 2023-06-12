ebaeef4267088b8e2f7fe00d37af65065ca98cff
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-06-12 15:52:55 -0500

    Headers añadidos a la solicitud

--- src/utils/fetchEvents.js
+++ src/utils/fetchEvents.js
@@ -2,9 +2,14 @@ import axios from "axios";
 
 export const fetchEvents = async () => {
   try {
-    const response = await axios.get('http://localhost:5000/api/evento/obtenerC'); // Ruta de la API en el backend
+    const response = await axios.get('http://localhost:5000/api/evento/obtenerC', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: localStorage.getItem('token')
+      }
+    }); // Ruta de la API en el backend
     return response.data.rows; // Almacenar los datos en el estado local
   } catch (error) {
-    console.error('Error al realizar la solicitud al backend:', error);
+    console.error('Error al realizar la solicitud al backend:', error.message);
   }
 };
