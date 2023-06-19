fd37425c429952fb432004352b576abad25eb654
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-06-19 17:23:53 -0500

    Centrado basado en la ciudad y scroll con rueda del ratón

--- src/components/Dashboard/MapView.jsx
+++ src/components/Dashboard/MapView.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import {
   MapContainer,
   TileLayer,
@@ -9,42 +9,39 @@ import {
 } from "react-leaflet";
 import ButtonAddEventAndPlace from "./ButtonAddEventsAndPlaces/ButtonAddEventAndPlace"
 import { getPlaceName } from "../../utils/placeName.js";
+import { UserDataContext } from "../Profile/UserDataProvider";
 
 export default function MapView() {
 
+  // Datos del usuario para sacar la ciudad
+  const { userData } = useContext(UserDataContext)
+
+  // Ubicaciones predeterminadas
+  const defaultLocations = {
+    111: {
+      coordinates: [4.0864122, -76.1909629],
+      zoom: 14
+    },
+    222: {
+      coordinates: [3.4348269, -76.5041975],
+      zoom: 13
+    }
+  }
+
+  // Estado para obtener ubicación del usuario con base en su ciudad
   const [userLocation, setUserLocation] = useState(null)
 
 
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setUserLocation([latitude, longitude]);
-        },
-        (error) => {
-          console.error("Error obteniendo la ubicacion:", error);
-        },
-        { enableHighAccuracy: true }
-      );
-    } else {
-      console.error("La geolocalización no está habilitada para este navegador.");
+    if (userData) {
+      setUserLocation({ coordinates: defaultLocations[userData.ciudad].coordinates, zoom: defaultLocations[userData.ciudad].zoom })
     }
-  }, []);
-  
+  }, [userData]);
+
 
   const [isModalOpen, setModalOpen] = useState(false);
 
   const [coord, setCoord] = useState(null)
-
-
-  const openModal = () => {
-    setModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalOpen(false);
-  };
 
   // Marcadores iniciales (los que el usuario tenga guardados)
   const [markers, setMarkers] = useState([
@@ -74,71 +71,71 @@ export default function MapView() {
     <div className="lg:basis-[70%] z-0">
       {userLocation && (
         <MapContainer
-        center={userLocation}
-        zoom={17}
-        scrollWheelZoom={false}
-        className="h-[63vh] lg:h-[89vh] shadow-[10px_10px_22px_-13px_rgba(0,0,0,0.4)]"
-      >
-        {/* Función para pintar los marcadores */}
-        {markers.map((marker) => (
-          <Marker
-            key={marker.key}
-            position={[marker.coordinates[0], marker.coordinates[1]]}
-          // eventHandlers={{
-          //   click: () => {
-          //     onclick = { openModal }
-          //   },
-          // }}
-          >
-            <Popup >
-              <div className="w-full h-auto flex items-center justify-center z-50">
-                <div className="bg-white rounded-lg p-6">
-                  <h2 className="text-lg font-bold mb-4">
-                    Informacion del sitio
-                  </h2>
-                  <p className="mb-4">Petición a backend en progreso...</p>
+          center={userLocation.coordinates}
+          zoom={userLocation.zoom}
+          scrollWheelZoom={false}
+          className="h-[63vh] lg:h-[89vh] shadow-[10px_10px_22px_-13px_rgba(0,0,0,0.4)]"
+        >
+          {/* Función para pintar los marcadores */}
+          {markers.map((marker) => (
+            <Marker
+              key={marker.key}
+              position={[marker.coordinates[0], marker.coordinates[1]]}
+            // eventHandlers={{
+            //   click: () => {
+            //     onclick = {  }
+            //   },
+            // }}
+            >
+              <Popup >
+                <div className="w-full h-auto flex items-center justify-center z-50">
+                  <div className="bg-white rounded-lg p-6">
+                    <h2 className="text-lg font-bold mb-4">
+                      Informacion del sitio
+                    </h2>
+                    <p className="mb-4">Petición a backend en progreso...</p>
+                  </div>
                 </div>
-              </div>
-            </Popup>
-          </Marker>
-        ))}
+              </Popup>
+            </Marker>
+          ))}
 
-        {/* Posición inicial en el mapa */}
-        <Circle center={[4.074862, -76.192516]} radius={20} />
-        {/* Atribución */}
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        {/* Botón para añadir un evento */}
-        <ButtonAddEventAndPlace
-          isToggled={isToggled}
-          isToggledMarker={isToggledMarker}
-          isToggledDate={isToggledDate}
-          setIsToggledDate={setIsToggledDate}
-          setIsToggled={setIsToggled}
-          setIsToggledMarker={setIsToggledMarker}
-          placeName={placeName}
-          setPlaceName={setPlaceName}
-          selectedDate={selectedDate}
-          setSelectedDate={setSelectedDate}
-          placeNameDate={newEventDate} register
-          setPlaceNameDate={setNewEventDate}
-          coord={coord}
-        />
-        {/* Componente para actualizar la lista de marcadores */}
-        {isToggledMarker && (
-          <AddMarker
-            setMarkers={setMarkers}
-            isToggledMarker={isToggledMarker}
-            setIsToggledMarker={setIsToggledMarker}
-            setPlaceName={setPlaceName}
-            setCoord={setCoord}
+          {/* Posición inicial en el mapa */}
+          <Circle center={[4.074862, -76.192516]} radius={20} />
+          {/* Atribución */}
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
-        )}
-      </MapContainer>
+          {/* Botón para añadir un evento */}
+          <ButtonAddEventAndPlace
+            isToggled={isToggled}
+            isToggledMarker={isToggledMarker}
+            isToggledDate={isToggledDate}
+            setIsToggledDate={setIsToggledDate}
+            setIsToggled={setIsToggled}
+            setIsToggledMarker={setIsToggledMarker}
+            placeName={placeName}
+            setPlaceName={setPlaceName}
+            selectedDate={selectedDate}
+            setSelectedDate={setSelectedDate}
+            placeNameDate={newEventDate} register
+            setPlaceNameDate={setNewEventDate}
+            coord={coord}
+          />
+          {/* Componente para actualizar la lista de marcadores */}
+          {isToggledMarker && (
+            <AddMarker
+              setMarkers={setMarkers}
+              isToggledMarker={isToggledMarker}
+              setIsToggledMarker={setIsToggledMarker}
+              setPlaceName={setPlaceName}
+              setCoord={setCoord}
+            />
+          )}
+        </MapContainer>
       )}
-      
+
     </div>
   );
 }
