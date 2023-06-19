b3d4ee7e04fa9974466af266e2675a18f8a73e13
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-06-19 16:10:00 -0500

    Altura del footer ajustada

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     // Contenedor del footer
-    <footer className="absoulte bottom-0 bg-gray-800 p-2 ">
+    <footer className="absoulte bottom-0 bg-gray-800 p-2 py-6 ">
 
       {/* Contenedor para limitar el ancho del contenido del footer */}
       <div className="flex justify-center min-[506px]:justify-between flex-wrap min-[824px]:flex-nowrap items-center text-left lg:text-left w-full "> {/* [&>div]:border-2 [&>ul]:border-2 */}
