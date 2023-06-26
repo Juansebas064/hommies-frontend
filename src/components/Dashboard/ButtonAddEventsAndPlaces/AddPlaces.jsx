410912616eeaa597474936ff4843dda79cee8799
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-06-25 22:25:18 -0500

    Rediseño del file chooser

--- src/components/Dashboard/ButtonAddEventsAndPlaces/AddPlaces.jsx
+++ src/components/Dashboard/ButtonAddEventsAndPlaces/AddPlaces.jsx
@@ -4,13 +4,14 @@ import { useForm } from "react-hook-form";
 import { PlacesContext } from "../Places/PlacesProvider";
 import { UserDataContext } from "../../Profile/UserDataProvider";
 
-const AddPlaces = ({ handleToggleMarker, placeName, markerAux, setMarkerAux, setIsToggled }) => {
+const AddPlaces = ({ handleToggleMarker, placeName, markerAux, setMarkerAux, setIsToggled, isToggledMarker }) => {
   const { fetchPlaces } = useContext(PlacesContext)
 
   const { userData } = useContext(UserDataContext)
 
   const [nombreCiudad, setNombreCiudad] = useState("");
   const [codigoCiudad, setCodigoCiudad] = useState("");
+  const [nombreImagen, setNombreImagen] = useState('Selecionar una imagen')
 
   useEffect(() => {
     if (userData.ciudad) {
@@ -65,7 +66,7 @@ const AddPlaces = ({ handleToggleMarker, placeName, markerAux, setMarkerAux, set
   };
 
   return userData ? (
-    <div className="w-full px-3 mb-4 mt-3 items-center relative">
+    <div className={`w-full px-3 mb-4 mt-3 items-center relative ${isToggledMarker ? 'h-6' : 'h-auto'} lg:h-auto`}>
       <div className="w-full items-center text-center justify-center pb-3">
         <form onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
           <div className="mb-4">
@@ -89,19 +90,21 @@ const AddPlaces = ({ handleToggleMarker, placeName, markerAux, setMarkerAux, set
           {errors.nombre?.type === "required" && (
             <p>Campo nombre es requerido *</p>
           )}
+
+          {/* Dirección */}
           <label className="mb-2 text-xs font-semibold px-1">Dirección</label>
-          <div className="flex items-center">
+          <div className="flex justify-between gap-1">
             <input
               type="text"
               placeholder="Selecciona una dirección"
-              className="w-[88%] px-4 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+              className="w-[88%] px-4 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500 flex-grow"
               disabled={true}
               value={placeName}
               id="direccion"
             />
 
             <button
-              className="relative w-[10%] rounded-md border-2 border-gray-200 outline-none hover:border-indigo-500 focus:border-indigo-500 hover:duration-200"
+              className="relative px-1 rounded-md border-2 border-gray-200 outline-none hover:border-indigo-500 focus:border-indigo-500 hover:duration-200"
               onClick={(e) => {
                 e.preventDefault()
                 handleToggleMarker()
@@ -172,18 +175,35 @@ const AddPlaces = ({ handleToggleMarker, placeName, markerAux, setMarkerAux, set
             value={nombreCiudad}
           />
 
+          {/* Imágen */}
 
           <label className="mb-3 mt-3 text-xs font-semibold px-1">Imagen</label>
-          <input
-            type="file"
-            className="w-full px-4 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
-            id="foto"
-            name="foto"
-            multiple={false}
-          />
+
+          <div className="h-[49px] flex items-center mt-2 relative bg-white cursor-pointer rounded-lg shadow-[10px_10px_22px_-13px_rgba(0,0,0,0.4)] active:border-indigo-500 overflow-hidden hover:bg-[rgba(255,255,255,0.5)]">
+            <input
+              type="file"
+              className="absolute top-0 right-0 left-0 bottom-0 w-full z-[-10] py-2 block mx-auto rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500 opacity-0"
+              id="foto"
+              capture='capture'
+              name="foto"
+              onChange={(e) => {
+                const nombre = e.target.files[0].name
+                setNombreImagen(nombre)
+              }}
+              multiple={false}
+            />
+            <span className=" bg-indigo-500 h-full flex items-center justify-center px-3">
+              <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" fill="#ffffff" clipRule="evenodd">
+                <path d="M9 16h-8v6h22v-6h-8v-1h9v8h-24v-8h9v1zm11 2c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1zm-7.5 0h-1v-14.883l-4.735 5.732-.765-.644 6.021-7.205 5.979 7.195-.764.645-4.736-5.724v14.884z" />
+              </svg>
+            </span>
+            <span className="text-left sm:text-center max-w-[425px] flex-grow whitespace-nowrap text-ellipsis px-2 overflow-hidden">
+              {nombreImagen}
+            </span>
+          </div>
           <button
             type="submit"
-            className="bg-indigo-500 rounded-xl mt-3 w-1/3 font-bold text-white text-sm hover:w-2/3 hover:duration-100 hover:bg-indigo-700"
+            className="bg-indigo-500 rounded-3xl mt-5 py-2 px-3 font-bold text-white text-sm hover:w hover:duration-100 hover:bg-indigo-700"
           >
             Crear lugar
           </button>
