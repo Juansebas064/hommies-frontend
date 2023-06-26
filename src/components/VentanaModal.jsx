58e84fa8056765b03e94bf870a05eb5d78363209
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-06-26 09:24:56 -0500

    Botón de cerrar siempre visible

--- src/components/VentanaModal.jsx
+++ src/components/VentanaModal.jsx
@@ -21,13 +21,15 @@ const VentanaModal = ({ children, estado, cambiarEstado }) => {
 
           {/* Card */}
           <div className="z-[9999] bg-white rounded-[14px] relative min-w-[250px] max-w-[370px] sm:max-w-[470px] max-h-[70vh] sm:max-h-[70vh] px-3 sm:px-7 flex-grow overflow-auto">
-            <span onClick={() => {
-              document.body.classList.remove('overflow-hidden');
-              cambiarEstado(false)
-            }}
-              className="z-[9999] text-base font-bold absolute top-2 right-2 rounded-3xl cursor-pointer hover:scale-[0.8] hover:transition-all duration-500 p-1 hover:bg-red-500">
-              {close(22)}
-            </span>
+            <div className="sticky top-0 translate-x-[30px]">
+              <span onClick={() => {
+                document.body.classList.remove('overflow-hidden');
+                cambiarEstado(false)
+              }}
+                className="z-[9999] text-base font-bold absolute top-2 right-2 rounded-3xl cursor-pointer hover:scale-[0.8] hover:transition-all duration-500 p-1 hover:bg-red-500">
+                {close(22)}
+              </span>
+            </div>
             {children}
           </div>
         </div>
