2306cce12cbc2c23ff0bb9c3c11c60aabbb73b4e
casariz <danielfelipedfco@gmail.com>
2023-07-01 22:52:01 -0500

    Integración de mostrar u ocultar contraseña

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,101 +1,108 @@
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { GoogleLogin } from '@react-oauth/google';
-import axios from 'axios';
-import { useContext, useEffect, useState } from 'react';
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { UserDataContext } from './Profile/UserDataProvider';
-import ErrorLogin from './VentanaModal';
+import { UserDataContext } from "./Profile/UserDataProvider";
+import ErrorLogin from "./VentanaModal";
 
 const Login = () => {
-
   // Función para consultar info del usuario en la bd cuando se haga login
-  const { userData, getUserDataFromDB } = useContext(UserDataContext)
+  const { userData, getUserDataFromDB } = useContext(UserDataContext);
 
   //Obtencíon de datos con react hook form
-  const { register, formState: { errors }, handleSubmit } = useForm();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
   // Redirección con base en si tiene o no el perfil completo
   useEffect(() => {
     if (userData) {
-      (!userData.genero || !userData.fecha_nacimiento || !userData.ciudad) ? window.location.href = "/profile/config" : window.location.href = "/dashboard"
+      !userData.genero || !userData.fecha_nacimiento || !userData.ciudad
+        ? (window.location.href = "/profile/config")
+        : (window.location.href = "/dashboard");
     }
-  }, [userData])
+  }, [userData]);
 
   // Login con Google
   const handleLoginSuccess = async (response) => {
-
     console.log(response);
-    //devuelve el JWT 
-    await axios.post('http://localhost:5000/api/login/verify/google', {
+    //devuelve el JWT
+    await axios
+      .post("http://localhost:5000/api/login/verify/google", {
+        data: response,
+      })
+      .then((response) => {
+        console.log(response.data);
 
-      data: response
-    }).then((response) => {
-
-      console.log(response.data);
-
-      if (response.data.token != null) {
-
-        localStorage.setItem('token', response.data.token);
-        console.log("la respuesta entro y es: " + response.data.token);
-        getUserDataFromDB()
-      }
-      else {
-        console.log("la respuesta no entro y es: " + response.data);
-        window.location.href = '/register';
-      }
-    }).catch((error) => {
-      console.error(error);
-    });
-  }
-
+        if (response.data.token != null) {
+          localStorage.setItem("token", response.data.token);
+          console.log("la respuesta entro y es: " + response.data.token);
+          getUserDataFromDB();
+        } else {
+          console.log("la respuesta no entro y es: " + response.data);
+          window.location.href = "/register";
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   // Verificar login con nickname y contraseña
   const onSubmit = async (data) => {
     try {
-
       // Petición para buscar el usuario en la bd y generar el token JWT
-      const response = await axios.post('http://localhost:5000/api/create/JWT', data);
+      const response = await axios.post(
+        "http://localhost:5000/api/create/JWT",
+        data
+      );
 
       // Si se generó el token:
       if (response.data.token) {
-
-        localStorage.setItem('token', response.data.token); // Token a localStorage
-        getUserDataFromDB() // Obtención de los datos del usuario
+        localStorage.setItem("token", response.data.token); // Token a localStorage
+        getUserDataFromDB(); // Obtención de los datos del usuario
       }
       // Si no se generó el token
       else {
         console.log("la respuesta es: " + response.data.token);
-        console.log("tienes algo mal en la contraseña, o usuario O no estas registrado");
+        console.log(
+          "tienes algo mal en la contraseña, o usuario O no estas registrado"
+        );
         cambiarEstadoErrorLogin(true);
       }
     } catch (error) {
       // Aquí puedes manejar los errores que ocurran durante la solicitud
       console.error(error);
     }
-  }
-
+  };
 
   const [estadoErrorLogin, cambiarEstadoErrorLogin] = useState(false);
 
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     // Contenedor principal
-    <div className="flex flex-col justify-center items-center py-5 lg:min-h-[89vh]" >
-
+    <div className="flex flex-col justify-center items-center py-5 lg:min-h-[89vh]">
       {/* Contenedor del formulario (card) */}
 
-      <div className="px-10 pt-10 pb-0 bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-[80%] mx-5 max-w-[500px] overflow-hidden" >
+      <div className="px-10 pt-10 pb-0 bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-[80%] mx-5 max-w-[500px] overflow-hidden">
         <form onSubmit={handleSubmit(onSubmit)}>
           {/* Title */}
-          <h1 className="font-bold text-3xl text-gray-900 text-center mb-7" >
+          <h1 className="font-bold text-3xl text-gray-900 text-center mb-7">
             Hommies
           </h1>
 
           {/* Div nickname */}
           <div className="flex -mx-3">
             <div className="w-full px-3 mb-5">
-              <label className="text-sm font-semibold px-1">
-                Nickname
-              </label>
+              <label className="text-sm font-semibold px-1">Nickname</label>
 
               <div className="flex">
                 <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
@@ -118,12 +125,14 @@ const Login = () => {
                   type="text"
                   className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                   placeholder="alejogonzales05"
-                  {...register('nickname', {
-                    required: true
+                  {...register("nickname", {
+                    required: true,
                   })}
                 />
               </div>
-              {errors.nickname?.type === 'required' && <p>Campo nickname es requerido *</p>}
+              {errors.nickname?.type === "required" && (
+                <p>Campo nickname es requerido *</p>
+              )}
             </div>
           </div>
 
@@ -131,9 +140,7 @@ const Login = () => {
           <div className="flex flex-col -mx-3">
             <div className="w-full px-3 mb-3">
               <div className="flex items-center justify-between">
-                <label className="text-sm font-semibold px-1">
-                  Contraseña
-                </label>
+                <label className="text-sm font-semibold px-1">Contraseña</label>
                 <a
                   href="/recoverpassword"
                   className="text-sm font-semibold text-indigo-500 hover:text-indigo-700 text-right"
@@ -142,7 +149,7 @@ const Login = () => {
                 </a>
               </div>
 
-              <div className="flex">
+              <div className="flex relative">
                 <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -160,15 +167,50 @@ const Login = () => {
                   </svg>
                 </div>
                 <input
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                   placeholder="************"
-                  {...register('password', {
-                    required: true
+                  {...register("password", {
+                    required: true,
                   })}
                 />
+                <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
+                  {showPassword ? (
+                    <svg
+                      className={`h-6 ${
+                        showPassword ? "text-gray-700" : "text-indigo-500"
+                      }`}
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 640 512"
+                      onClick={togglePasswordVisibility}
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M320 400c-75.85 0-137.25-58.71-142.9-133.11L72.2 185.82c-13.79 17.3-26.48 35.59-36.72 55.59a32.35 32.35 0 0 0 0 29.19C89.71 376.41 197.07 448 320 448c26.91 0 52.87-4 77.89-10.46L346 397.39a144.13 144.13 0 0 1-26 2.61zm313.82 58.1l-110.55-85.44a331.25 331.25 0 0 0 81.25-102.07 32.35 32.35 0 0 0 0-29.19C550.29 135.59 442.93 64 320 64a308.15 308.15 0 0 0-147.32 37.7L45.46 3.37A16 16 0 0 0 23 6.18L3.37 31.45A16 16 0 0 0 6.18 53.9l588.36 454.73a16 16 0 0 0 22.46-2.81l19.64-25.27a16 16 0 0 0-2.82-22.45zm-183.72-142l-39.3-30.38A94.75 94.75 0 0 0 416 256a94.76 94.76 0 0 0-121.31-92.21A47.65 47.65 0 0 1 304 192a46.64 46.64 0 0 1-1.54 10l-73.61-56.89A142.31 142.31 0 0 1 320 112a143.92 143.92 0 0 1 144 144c0 21.63-5.29 41.79-13.9 60.11z"
+                      ></path>
+                    </svg>
+                  ) : (
+                    <svg
+                      className={`h-6 ${
+                        showPassword ? "text-indigo-500" : "text-gray-700"
+                      }`}
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 576 512"
+                      onClick={togglePasswordVisibility}
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z"
+                      ></path>
+                    </svg>
+                  )}
+                </div>
               </div>
-              {errors.password?.type === 'required' && <p>Campo contraseña es requerido *</p>}
+              {errors.password?.type === "required" && (
+                <p>Campo contraseña es requerido *</p>
+              )}
             </div>
           </div>
 
@@ -183,23 +225,26 @@ const Login = () => {
 
             <ErrorLogin
               estado={estadoErrorLogin}
-              cambiarEstado={cambiarEstadoErrorLogin}>
-              <h1 className="text-center text-xl font-semibold mt-9 mb-3">¡Error al intentar iniciar sesión!</h1>
-              <p className='text-center mb-5'>Por favor verifíca que la información de tu nickname y/o contraseña estén bien.</p>
+              cambiarEstado={cambiarEstadoErrorLogin}
+            >
+              <h1 className="text-center text-xl font-semibold mt-9 mb-3">
+                ¡Error al intentar iniciar sesión!
+              </h1>
+              <p className="text-center mb-5">
+                Por favor verifíca que la información de tu nickname y/o
+                contraseña estén bien.
+              </p>
             </ErrorLogin>
 
-            <div className="text-center mt-4 ">
-              - O continua con -
-            </div>
+            <div className="text-center mt-4 ">- O continua con -</div>
 
             {/* Botón de Google */}
             <button className="rounded-full flex items-center justify-center border-1 border-gray-200 hover:bg-gray-200 w-13 max-w-xs mx-auto mt-4 text-black px-1 font-normal">
-
               <GoogleOAuthProvider clientId="650698705793-eetbgpi48lld1tb4eol6n8g355raapeg.apps.googleusercontent.com">
                 <GoogleLogin
-                  type='icon'
-                  shape='circle'
-                  size='large'
+                  type="icon"
+                  shape="circle"
+                  size="large"
                   onSuccess={handleLoginSuccess}
                   onError={() => {
                     console.log("Login Failed");
@@ -211,7 +256,6 @@ const Login = () => {
         </form>
       </div>
 
-
       {/* No tienes cuenta??? */}
       <div className="bg-gray-100 text-gray-500 rounded-3xl shadow-xl overflow-hidden mt-8 px-4 py-3">
         <p className="font-bold text-semibold text-gray-900">
@@ -219,15 +263,14 @@ const Login = () => {
           <a
             href="/register"
             className="text-semibold font-bold text-indigo-500 hover:text-indigo-700"
-          > Crea una
+          >
+            {" "}
+            Crea una
           </a>
         </p>
       </div>
-
     </div>
-
-
-  )
+  );
 };
 
 export default Login;
